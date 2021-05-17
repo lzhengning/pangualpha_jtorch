cfc93d28ac81fcdddd059f081c668090255d0fcb
yands <273471520@qq.com>
2021-05-17 15:54:09 +0800

    fist commit

---
+++ inference/eval_task-2.6b-fp16.py
@@ -0,0 +1,77 @@
+import mindspore.nn as nn
+from mindspore import load_checkpoint, load_param_into_net
+from mindspore.train.model import Model
+import mindspore as ms
+from pangu_dropout_recompute_eos_fp16 import PANGUALPHA,EvalNet
+from utils_fix import PANGUALPHAConfig
+
+from tokenization_jieba import JIEBATokenizer
+from generate import generate
+import os
+import numpy as np
+
+
+def integrate_ckpt_file():
+
+    ms.context.set_context(save_graphs=False, mode=ms.context.GRAPH_MODE, device_target="GPU")
+
+    eod_reset = False
+    model_parallel_num = 1
+    data_parallel_num = int(1 / model_parallel_num)
+    per_batch_size = 1
+    batch_size = per_batch_size * data_parallel_num
+
+    config = PANGUALPHAConfig(
+        data_parallel_num=data_parallel_num,
+        model_parallel_num=model_parallel_num,
+        batch_size=batch_size,
+        seq_length=1024,
+        vocab_size=40000,
+        embedding_size=2560,  # 353M   8B
+        num_layers=32,
+        num_heads=32,
+        expand_ratio=4,
+        post_layernorm_residual=False,
+        dropout_rate=0.1,
+        compute_dtype=ms.float16,
+        use_past=False,
+        self_layernorm=True,
+        forward_reduce_scatter=True,
+        word_emb_dp=True,
+        eod_reset=eod_reset)
+
+    pangualpha = PANGUALPHA(config)
+    print('initial PANGU-ALPHA success!!!')
+
+    eval_net = EvalNet(pangualpha)
+    eval_net.set_train(False)
+    model_predict = Model(eval_net)
+
+    param_dict = load_checkpoint('/userhome/temp/2.6b_ckpts/new.ckpt')
+    load_param_into_net(eval_net, param_dict)
+
+    print('load_param_into_net success!!!!!!!!')
+    print("================load param ok=================", flush=True)
+
+    tokenizer_path = os.getcwd() + "/tokenizer"
+    tokenizer = JIEBATokenizer(os.path.join(tokenizer_path, 'vocab.vocab'),
+                               os.path.join(tokenizer_path, 'vocab.model'))
+
+    while 1:
+        sample = input("Tell Pangu-alpha what you want to generate:")
+        tokenized_token = tokenizer.tokenize(sample)
+        start_sentence = tokenizer.convert_tokens_to_ids(tokenized_token)
+        input_ids = np.array(start_sentence).reshape(1, -1)
+        output_ids = generate(model_predict, input_ids, config.seq_length, 9)
+        output_samples = tokenizer.convert_ids_to_tokens(output_ids.tolist())
+        print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+        print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+        print('Input is:', sample)
+        print('Output is:', output_samples, flush=True)
+
+    return
+
+if __name__ == "__main__":
+    integrate_ckpt_file()
+
+
---
+++ inference/generate.py
@@ -0,0 +1,79 @@
+"""
+TopK for text generation
+"""
+
+import numpy as np
+import mindspore.common.dtype as mstype
+from mindspore.common.tensor import Tensor
+import mindspore as ms
+
+
+def top_k_logits(logits, top_k=0, top_p=0.9, filter_value=-float(0)):
+    """ This function has been mostly taken from huggingface conversational
+     ai code at
+         https://medium.com/huggingface/how-to-build-a-state-of-the-art-
+              conversational-ai-with-transfer-learning-2d818ac26313 """
+    if top_k > 0:
+        # Remove all tokens with a probability less than the
+        # last token of the top-k
+
+        p_args = logits.argsort()[::-1][:top_k]
+        mask = np.ones(logits.shape) * filter_value
+        mask[p_args] = 1
+        logits = logits * mask
+
+    if top_p > 0.0:
+        # Cconvert to 1D
+        sorted_indices = np.argsort(logits, axis=-1)[::-1]
+        sorted_logits = logits[sorted_indices]
+
+        # cumulative_probs = np.cumsum(softmax(sorted_logits), axis=-1)
+        cumulative_probs = np.cumsum(sorted_logits, axis=-1)
+
+        # Remove tokens with cumulative probability above the threshold
+        sorted_indices_to_remove = cumulative_probs > top_p
+        # Shift the indices to the right to keep also the first token
+        # above the threshold
+        sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1]
+        sorted_indices_to_remove[..., 0] = 0
+        indices_to_remove = sorted_indices[sorted_indices_to_remove]
+        logits[indices_to_remove] = filter_value
+    return logits
+
+def softmax(x):
+    """Compute the softmax in a numerically stable way."""
+    x = x - np.max(x)
+    exp_x = np.exp(x)
+    softmax_x = exp_x / np.sum(exp_x)
+    return softmax_x
+
+def generate(model, origin_inputs, seq_length, end_token=50256):
+    pad_length = seq_length - origin_inputs.shape[-1]
+
+    generate_tokens_num = 0
+    valid_length = len(origin_inputs)
+    label_token_length = 50
+    input_ids = np.pad(origin_inputs, ((0, 0), (0, pad_length)), 'constant', constant_values=(0, 0))
+    while generate_tokens_num < label_token_length:
+        logits = model.predict(ms.Tensor(input_ids, ms.int32)).asnumpy().reshape(1, seq_length, -1)
+        probs = top_k_logits(logits[0, valid_length - 1], top_k=0, top_p=0.8)
+        p = softmax(probs)
+        p = p / sum(p)
+        target_index = np.random.choice(len(p), p=p)
+        #         print(target_index)
+
+        if (target_index == end_token) or (valid_length == seq_length - 1):
+            outputs = input_ids
+            break
+
+        if not target_index == 0:  # 后处理，不允许模型生成UNK
+            input_ids[0][valid_length] = target_index
+            valid_length += 1
+            generate_tokens_num += 1
+        outputs = input_ids
+    outputs = outputs[0, :np.sum(outputs != 0)]
+    return outputs
+    # print(logits.shape)
+    # print(logits)
+    # return origin_inputs
+
