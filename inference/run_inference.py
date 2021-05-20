025455278acfd6848f9005dde0db850269bf60ca
yands <273471520@qq.com>
2021-05-20 09:14:13 +0800

    add 2b6 fp32

--- inference/run_inference.py
+++ inference/run_inference.py
@@ -8,10 +8,12 @@ import os
 import numpy as np
 import argparse
 
-from pangu_dropout_recompute_eos_fp16 import EvalNet, PANGUALPHA, EvalNet_p
+from pangu_dropout_recompute_eos_fp16 import PANGUALPHA as PANGUALPHA_fp16
+from pangu_dropout_recompute_eos_fp16 import EvalNet_p
 from pangu_wrapcell_gradient_scale_eos import VirtualDatasetOneInputCell
 from utils_fix import PANGUALPHAConfig
 
+from pangu_dropout_recompute_eos import PANGUALPHA as PANGUALPHA_fp32
 
 def get_model_13b_fp16(args):
     # model_parallel_num = 8
@@ -38,7 +40,7 @@ def get_model_13b_fp16(args):
         word_emb_dp=True,
         eod_reset=False)
     print("===config is: ", config, flush=True)
-    pangu = PANGUALPHA(config)
+    pangu = PANGUALPHA_fp16(config)
     pangu_ = VirtualDatasetOneInputCell(pangu)
     eval_pangu = EvalNet_p(pangu_, generate=True)
     eval_pangu.set_train(False)
@@ -77,22 +79,60 @@ def get_model_2b6_fp16(args):
         forward_reduce_scatter=True,
         word_emb_dp=True,
         eod_reset=eod_reset)
+    print("===config is: ", config, flush=True)
 
-    pangualpha = PANGUALPHA(config)
-    print('initial PANGU-ALPHA success!!!')
-
-    eval_net = EvalNet(pangualpha)
-    eval_net.set_train(False)
-    model_predict = Model(eval_net)
+    pangu = PANGUALPHA_fp16(config)
+    pangu_ = VirtualDatasetOneInputCell(pangu)
+    eval_pangu = EvalNet_p(pangu_, generate=True)
+    eval_pangu.set_train(False)
+    model = Model(eval_pangu)
 
     param_dict = load_checkpoint(args.load_ckpt_path)
-    load_param_into_net(eval_net, param_dict)
+    load_param_into_net(eval_pangu, param_dict)
 
-    print('load_param_into_net success!!!!!!!!')
-    print("================load param ok=================", flush=True)
+    print('#### Load ckpt success!!! ####')
+    return model
 
-    return model_predict
 
+def get_model_2b6(args):
+
+    eod_reset = False
+    model_parallel_num = 1
+    data_parallel_num = int(1 / model_parallel_num)
+    per_batch_size = 1
+    batch_size = per_batch_size * data_parallel_num
+
+    config = PANGUALPHAConfig(
+        data_parallel_num=data_parallel_num,
+        model_parallel_num=model_parallel_num,
+        batch_size=batch_size,
+        seq_length=1024,
+        vocab_size=40000,
+        embedding_size=2560,  # 353M   8B
+        num_layers=32,
+        num_heads=32,
+        expand_ratio=4,
+        post_layernorm_residual=False,
+        dropout_rate=0.1,
+        compute_dtype=ms.float16,
+        use_past=False,
+        self_layernorm=True,
+        forward_reduce_scatter=True,
+        word_emb_dp=True,
+        eod_reset=eod_reset)
+    print("===config is: ", config, flush=True)
+
+    pangu = PANGUALPHA_fp32(config)
+    pangu_ = VirtualDatasetOneInputCell(pangu)
+    eval_pangu = EvalNet_p(pangu_, generate=True)
+    eval_pangu.set_train(False)
+    model = Model(eval_pangu)
+
+    param_dict = load_checkpoint(args.load_ckpt_path)
+    load_param_into_net(eval_pangu, param_dict)
+
+    print('#### Load ckpt success!!! ####')
+    return model
 
 def run_eval(args):
 
@@ -109,8 +149,20 @@ def run_eval(args):
     tokenizer = JIEBATokenizer(os.path.join(tokenizer_path, 'vocab.vocab'),
                                os.path.join(tokenizer_path, 'vocab.model'))
 
-    while 1:
-        sample = input("Tell Pangu-alpha what you want to generate:")
+    samples = ['上联：瑞风播福泽，事业具昌盛千家乐',
+               '四川的省会是?',
+               '上联：上联春雨润人间，社会和谐万象新',
+               '''书生：羌笛何须怨杨柳，春风不度玉门关。
+飞云：（这诗怎么这么耳熟？且过去跟他聊聊如何。）
+书生：小兄弟，要不要一起喝一杯？
+飞云：你请我呀？你若是请我，我便和你喝一杯；你若不请我，我便一个人去喝。
+书生：小兄弟，看你年纪轻轻，不至于这么势利吧？
+飞云：''',
+               '张无忌拿出屠龙宝刀，手起刀落，周芷若掉了一颗门牙，身旁的赵敏喜极而泣，',
+               '人工智能成为国际竞争的新焦点。人工智能是引领未来的战略性技术，世界主要发达国家把发展人工智能作为提升国家竞争力、维护国家安全的重大战略，加紧出台规划和政策，围绕核心技术、顶尖人才、标准规范等强化部署，力图在新一轮国际科技竞争中掌握主导权。当前，',
+               '中国和美国和日本和法国和加拿大和澳大利亚的首都分别是哪里？']
+    for sample in samples:
+        # sample = input("Tell Pangu-alpha what you want to generate:")
         tokenized_token = tokenizer.tokenize(sample)
         start_sentence = tokenizer.convert_tokens_to_ids(tokenized_token)
         input_ids = np.array(start_sentence).reshape(1, -1)
@@ -120,10 +172,13 @@ def run_eval(args):
         print('Input is:', sample)
         print('Output is:', output_samples, flush=True)
         print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-
     return
 
 if __name__ == "__main__":
+    #python run_inference.py --model=13B_fp16 --load_ckpt_path=/userhome/temp/PanguAlpha_13b_fp16.ckpt
+    #python run_inference.py --model=2B6_fp16 --load_ckpt_path=/userhome/temp/PanguAlpha_2_6b.ckpt
+    #python run_inference.py --model=2B6 --load_ckpt_path=/userhome/temp/PanguAlpha_2_6b.ckpt
+
     parser = argparse.ArgumentParser(description="PANGUALPHA predicting")
     parser.add_argument("--model",
                         type=str,
